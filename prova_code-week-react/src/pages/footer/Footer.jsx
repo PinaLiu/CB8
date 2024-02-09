fc87a67039c3321b04ff84bc48e5cc78132bbb04
Pina <119705587+PinaLiu@users.noreply.github.com>
2024-02-09 13:29:53 +0100

    Add files via upload

---
+++ prova_code-week-react/src/pages/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <div className={styles.container_prova}>
+        <div className={styles.footer}>
+          <h1>Footer</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Footer;
