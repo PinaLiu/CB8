6e09039946df6cacc295e60c8c1c56c22ad0af95
Pina <119705587+PinaLiu@users.noreply.github.com>
2024-02-09 13:57:20 +0100

    Delete prova_code-week-react directory

--- prova_code-week-react/src/pages/menu/Menu.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./menu.module.scss";
-
-fetch("/components/menu.json")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Errore:", error));
