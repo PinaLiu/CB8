374d1a6888f4c55ca350a7bf3b6913e70714b5af
Pina <119705587+PinaLiu@users.noreply.github.com>
2024-03-17 17:39:17 +0100

    deploy next.config.mjs

--- Web App Pizza no BE/next.config.mjs
+++ Web App Pizza no BE/next.config.mjs
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
   reactStrictMode: true,
   images: {
     remotePatterns: [{ hostname: "th.bing.com" }],
   },
+ //per  deploy su github pages
+  basePath: isProd ? '/CB8/Web%20App%20Pizza%20no%20BE' : '',
+  assetPrefix: isProd ? '/CB8/Web%20App%20Pizza%20no%20BE/' : '',
 };
 
 export default nextConfig;
